d3ab55398d81b0361355e2d4f7dd9300a3a57057
Paulina.Ratynska <paulina.ratynska1@gmail.com>
2022-09-05 15:38:27 +0200

    findMinYearInJson method

--- src/main/java/JsonParseDummyResponse.java
+++ src/main/java/JsonParseDummyResponse.java
@@ -6,8 +6,20 @@ public class JsonParseDummyResponse {
     public static void main(String[] args) {
         //mocked response
         JsonPath js = new JsonPath(payload.yearMultipliedByPopulation());
-        int numberOfYears = js.getInt("data.size()"); //size() used for arrays only, output expected is 8 as there are 8 items in array
-        System.out.println("How many years were used in the database: " + numberOfYears);
+        int numberOfElementsInJsonArray = js.getInt("data.size()"); //size() used for arrays only, output expected is 8 as there are 8 items in array
+        System.out.println("How many years were used in the database: " + numberOfElementsInJsonArray);
+
+        System.out.println("current min Year " + findMinYearInJson(js, numberOfElementsInJsonArray));
     }
 
+    private static int findMinYearInJson(JsonPath js, int numberOfYears) {
+        int currentMinYear = js.getInt("data[0].Year");   //Parent.Child in Json
+        for (int i = 0; i < numberOfYears; i++) {
+            int nextYearValue = js.getInt("data[" + ++i + "].Year");
+            if (currentMinYear > nextYearValue) {
+                currentMinYear = nextYearValue;
+            } else continue;
+        }
+        return currentMinYear;
+    }
 }
